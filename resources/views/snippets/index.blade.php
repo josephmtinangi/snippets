eb660fd94f69e112b0ab5dda0a39a8f3d352c5a7
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-03-18 10:49:23 +0300

    Show Fork Me link on the home page

--- resources/views/snippets/index.blade.php
+++ resources/views/snippets/index.blade.php
@@ -7,11 +7,15 @@
     @if(count($snippets))
         @foreach($snippets as $snippet)
             <article class="snippet">
-                <h4 class="title is-4">
-                    <a href="/snippets/{{ $snippet->id }}">
-                        {{ $snippet->title }}
-                    </a>
-                </h4>
+                <div class="is-flex">
+                    <h4 class="title is-4 flex">
+                        <a href="/snippets/{{ $snippet->id }}">
+                            {{ $snippet->title }}
+                        </a>
+                    </h4>   
+
+                    <a href="/snippets/{{ $snippet->id }}/fork">Fork Me</a>             
+                </div>
 
                 <pre>
                     <code>{{ $snippet->body }}</code>
