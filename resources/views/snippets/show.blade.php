a911d2f745c21d0334360761b054f21443c8a2ff
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-03-18 09:14:54 +0300

    Setup the layout

---
+++ resources/views/snippets/show.blade.php
@@ -0,0 +1,6 @@
+@component('layout')
+    @slot('title')
+        Snippets
+    @endslot
+<h1>Index</h1>
+@endcomponent
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::resource('users', 'UsersController');
-
-Route::resource('users.questions', 'QuestionsController');
+Route::get('/', 'SnippetsController@index')->name('home');
+Route::get('/snippets/{snippet}', 'SnippetsController@show');
+Route::get('/snippets/create', 'SnippetsController@create');
+Route::post('/snippets', 'SnippetsController@store');
